2955562041ee5cfe64c6456fa2b92161602a483c
Szesz96 <szesza.ka@wp.pl>
2023-04-17 20:37:20 +0200

    news component added

---
+++ src/components/newsComponent/newsComponent.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import './news.css';
+import { Article } from '../../types/news';
+
+
+const NewsComponent: React.FC = () => {
+
+    const [news, setNews] = useState<Article[]>([]);
+
+    useEffect(() => {
+
+        const apiURL = `https://newsapi.org/v2/everything?q=sport&from=2023-04-01&sortBy=popularity&apiKey=ee1e6126ee11484fb715297f8fd1d85a`
+    
+        fetch(apiURL) 
+          .then(response => response.json())
+          .then(data => setNews(data.articles))
+          .catch(err => console.error(err));
+      }, []);
+    
+  return (
+      <div className='articles'>
+          {news.map(article => (
+            <div key={article.title} className='article'>
+              <img src={article.urlToImage} alt={article.title} className='art-img' />
+              <div className='art-text'>
+                <h2>{article.title}</h2>
+                <div className='br'></div>
+                <p>{article.description}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+  )
+}
+
+export default NewsComponent
--- src/views/news/news.tsx
+++ src/views/news/news.tsx
@@ -1,36 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './news.css';
 import { Article } from '../../types/news';
-
+import NewsComponent from '../../components/newsComponent/newsComponent';
 
 const NewsView: React.FC = () => {
+	return (
+		<>
+			<NewsComponent />
+		</>
+	);
+};
 
-    const [news, setNews] = useState<Article[]>([]);
-
-    useEffect(() => {
-
-        const apiURL = `https://newsapi.org/v2/everything?q=sport&from=2023-04-01&sortBy=popularity&apiKey=ee1e6126ee11484fb715297f8fd1d85a`
-    
-        fetch(apiURL) 
-          .then(response => response.json())
-          .then(data => setNews(data.articles))
-          .catch(err => console.error(err));
-      }, []);
-    
-  return (
-      <div className='articles'>
-          {news.map(article => (
-            <div key={article.title} className='article'>
-              <img src={article.urlToImage} alt={article.title} className='art-img' />
-              <div className='art-text'>
-                <h2>{article.title}</h2>
-                <div className='br'></div>
-                <p>{article.description}</p>
-              </div>
-            </div>
-          ))}
-      </div>
-  )
-}
-
-export default NewsView
+export default NewsView;
