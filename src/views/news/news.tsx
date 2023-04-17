5fdc513da8b47b92b618227bdd4580d88c06bc84
t1psyy <p.bankowski101@gmail.com>
2023-04-17 20:33:36 +0200

    fix change padding and visibility

--- src/views/news/news.tsx
+++ src/views/news/news.tsx
@@ -1,36 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import './news.css';
-import { Article } from '../../types/news';
-
+import React, { useEffect, useState } from "react";
+import "./news.css";
+import { Article } from "../../types/news";
 
 const NewsView: React.FC = () => {
+  const [news, setNews] = useState<Article[]>([]);
 
-    const [news, setNews] = useState<Article[]>([]);
+  useEffect(() => {
+    const apiURL = `https://newsapi.org/v2/everything?q=sport&from=2023-04-01&sortBy=popularity&apiKey=ee1e6126ee11484fb715297f8fd1d85a`;
 
-    useEffect(() => {
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles))
+      .catch((err) => console.error(err));
+  }, []);
 
-        const apiURL = `https://newsapi.org/v2/everything?q=sport&from=2023-04-01&sortBy=popularity&apiKey=ee1e6126ee11484fb715297f8fd1d85a`
-    
-        fetch(apiURL) 
-          .then(response => response.json())
-          .then(data => setNews(data.articles))
-          .catch(err => console.error(err));
-      }, []);
-    
   return (
-      <div className='articles'>
-          {news.map(article => (
-            <div key={article.title} className='article'>
-              <img src={article.urlToImage} alt={article.title} className='art-img' />
-              <div className='art-text'>
-                <h2>{article.title}</h2>
-                <div className='br'></div>
-                <p>{article.description}</p>
-              </div>
-            </div>
-          ))}
-      </div>
-  )
-}
+    <div className="articles">
+      {news.map((article) => (
+        <div key={article.title} className="article">
+          <img
+            src={article.urlToImage}
+            alt={article.title}
+            className="art-img"
+          />
+          <div className="art-text">
+            <h2>{article.title}</h2>
+            <div className="br"></div>
+            <p dangerouslySetInnerHTML={{ __html: article.description }}></p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default NewsView
+export default NewsView;
