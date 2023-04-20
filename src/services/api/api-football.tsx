d00b4acc9e5575b4b9551b9f013a72efe1eb5842
t1psyy <p.bankowski101@gmail.com>
2023-04-20 19:50:10 +0200

    add live match preview

--- src/services/api/api-football.tsx
+++ src/services/api/api-football.tsx
@@ -1,52 +1,63 @@
+import { LiveMatchesType } from "../../types/liveMatches";
 import { LeagueData } from "../../types/standings";
 
 const apiFootballDef = () => {
-    const getLeagues = async () => {
-        try {
-            
+  const getLeagues = async () => {
+    try {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "7e909719d899edf4d9bc9ccaf6028a9c",
+          "X-RapidAPI-Host": "v3.football.api-sports.io",
+        },
+      };
+      const response = await fetch(
+        "https://v3.football.api-sports.io/leagues",
+        options
+      );
+      const standings = await response.json();
+      return standings.response;
+    } catch (error) {}
+  };
 
-                const options = {
-                  method: 'GET',
-                  headers: {
-                    'X-RapidAPI-Key': '7e909719d899edf4d9bc9ccaf6028a9c',
-                    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-                  }
-                };
-                const response = await fetch('https://v3.football.api-sports.io/leagues', options)
-                const standings = await response.json();
-                return standings.response
-                
-             
-        }
-        catch (error) {}
-    }
+  const getStandings = async (
+    league: number,
+    season: number
+  ): Promise<LeagueData[] | undefined> => {
+    try {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "7e909719d899edf4d9bc9ccaf6028a9c",
+          "X-RapidAPI-Host": "v3.football.api-sports.io",
+        },
+      };
+      const response = await fetch(
+        `https://v3.football.api-sports.io/standings?league=${league}&season=${season}`,
+        options
+      );
+      const standings = await response.json();
+      return standings.response;
+    } catch (error) {}
+  };
 
-    const getStandings = async (
-      league:number,
-      season:number,
-    ):Promise<LeagueData[] | undefined> => {
-      try {
-            
-
-        const options = {
-          method: 'GET',
+  const getLiveMatches = async (): Promise<LiveMatchesType[] | undefined> => {
+    try {
+      const response = await fetch(
+        "https://v3.football.api-sports.io/fixtures?live=all",
+        {
           headers: {
-            'X-RapidAPI-Key': '7e909719d899edf4d9bc9ccaf6028a9c',
-            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-          }
-        };
-        const response = await fetch(`https://v3.football.api-sports.io/standings?league=${league}&season=${season}`, options)
-        const standings = await response.json()
-        return standings.response
-        
-     
-}
-catch (error) {}
+            "x-rapidapi-host": "v3.football.api-sports.io",
+            "x-rapidapi-key": "7e909719d899edf4d9bc9ccaf6028a9c",
+          },
+        }
+      );
+      const liveMatchesData = await response.json();
+      return liveMatchesData.response;
+    } catch (error) {}
+  };
 
-    }
-
-    return{getLeagues, getStandings} 
-
-}
+  return { getLeagues, getStandings, getLiveMatches };
+};
 
 export const ApiFootball = apiFootballDef();
