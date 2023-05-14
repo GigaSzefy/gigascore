fc12a20e526c00ecc4eea7ccca2eec33f13d6e50
t1psyy <p.bankowski101@gmail.com>
2023-05-14 21:02:09 +0200

    add redux to news

--- src/components/newsComponent/newsComponent.tsx
+++ src/components/newsComponent/newsComponent.tsx
@@ -1,37 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import './news.css';
-import { Article } from '../../types/news';
-import { apiNews } from '../../services/api/api-news';
-
+import React, { useEffect, useState } from "react";
+import "./news.css";
+import { Article } from "../../types/news";
+import { apiNews } from "../../services/api/api-news";
+import { useAppSelector } from "../../app/hooks";
+import { selectNews } from "../../slices/news-slice";
 
 const NewsComponent: React.FC = () => {
+  const news = useAppSelector(selectNews);
 
-    const [news, setNews] = useState<Article[]>([]);
-
-    useEffect(() => {
-        (async () => {
-            const newsData = await apiNews.getNews()
-            setNews(newsData)
-        })()
-      }, []);
-    
   return (
-      
-      <div className='articles'>
-          {news.map(article => (
-            <div key={article.title} className='article'>
-              <a className='news=link' href={article.url} target='_blank'>
-                <img src={article.urlToImage} alt={article.title} className='art-img' />
-                <div className='art-text'>
-                  <h2>{article.title}</h2>
-                  <div className='br'></div>
-                  <p>{article.description}</p>
-                </div>
-              </a>
+    <div className="articles">
+      {news.map((article) => (
+        <div key={article.title} className="article">
+          <a className="news=link" href={article.url} target="_blank">
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className="art-img"
+            />
+            <div className="art-text">
+              <h2>{article.title}</h2>
+              <div className="br"></div>
+              <p>{article.description}</p>
             </div>
-          ))}
-      </div>
-  )
-}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default NewsComponent
+export default NewsComponent;
--- src/views/news/news.tsx
+++ src/views/news/news.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import NewsComponent from '../../components/newsComponent/newsComponent';
+import React, { useEffect } from "react";
+import NewsComponent from "../../components/newsComponent/newsComponent";
+import { useAppDispatch } from "../../app/hooks";
+import { getNewsAsync } from "../../slices/news-slice";
 
 const NewsView: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const getNews = async (): Promise<void> => {
+    dispatch(getNewsAsync());
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
   return (
     <>
       <NewsComponent />
